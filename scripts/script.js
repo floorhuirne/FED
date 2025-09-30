8ecba95a21d89d32b725600a0129b759c3832a4b
floorhuirne <floorhuirne@gmail.com>
2025-09-30 23:10:28 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,13 @@
 // JavaScript Document
 console.log("hi");
+
+//HAMBURGER MENU
+// Met hulp van de huiswerkopdracht
+const openMenuBtn = document.querySelector('.openMenu');
+const sluitMenuBtn = document.querySelector('.closeMenu');
+const menu = document.querySelector('.menu');
+
+openMenuBtn.onclick = () => menu.hidden = false;
+sluitMenuBtn.onclick = () => menu.hidden = true;
+
+//------------
